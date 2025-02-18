59a2da3d9fead76fcf1c999a34bce871076a4d94
hiab gebremichael <hiab.gebremichael.stu.ocsb.ca>
2025-02-18 18:07:53 +0000

    Added area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Hiab Gebremichael
+# Date: Feb. 18th, 2025
+# Program Function .This program calculates the area and perimeter of a rectangle
+
+
+def main():
+    print("For a rectangle with length 5cm and width 3cm:")
+    print()
+    print("the area is: {} cm^2".format(5 * 3))
+    print("The perimeter is: {}cm".format(2 * (5 + 3)))
+
+
+if __name__ == "__main__":
+    main()
